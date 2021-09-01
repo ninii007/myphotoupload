6a4fb4c0a1d73c8fb77e2bcd54150c86c9c1b1e4
tarubu <artid.b@ubu.ac.th>
2021-09-01 12:56:49 +0700

    init commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import UploadScreen from './screens/UploadScreen';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <UploadScreen />
+    </>
+  );
+};
+export default App;
